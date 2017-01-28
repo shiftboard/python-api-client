830cb70e4d7914a4e5447df5674c76e6760edbc8
Kevin Fink <kevin@fink.com>
2017-01-28 13:59:38 -0800

    Overrode getData() so we get extended results. Added updateAccount() and delete().

--- shiftboard/account.py
+++ shiftboard/account.py
@@ -6,25 +6,54 @@ from result import *
 from workgroup import Workgroup
 from call import RPCServerError
 
+
 class Account(Result):
     """Represents a single account record"""
     name = 'account'
     name_plural = 'accounts'
+
+    def getData(self):
+        # extended = True needed to get user_type (admin, etc.).
+        result = self.session.apicall('account.get',
+                                      id=self['id'],
+                                      extended=True,
+                                      org_settings=True,
+                                      user_applications=True,
+                                      )
+        return result['result']
+
+    def updateAccount(self):
+        try:
+            kwargs = self.copy()
+            kwargs.pop('region', None)
+            result = self.session.apicall('account.update', **kwargs)
+            data = result['result']
+        except RPCServerError as error:
+            raise
+        return data
+
+    def delete(self):
+        """Remove account"""
+        try:
+            result = self.session.apicall('account.delete', id=self['id'])
+            return True
+        except:
+            return False
 
     def admin(self):
         """Returns true if user is a super-user"""
         return self.get('user_type', '') == 'admin'
 
     def workgroups(self):
-        result = self.session.apicall('account.listMemberships', 
-            select = { 'member': self['id'] }
-        )
+        result = self.session.apicall('account.listMemberships',
+                                      select={'member': self['id']}
+                                      )
         data = result['result']['workgroups']
         return [Workgroup(self.session, seed=workgroup) for workgroup in data]
 
     def image(self):
         try:
-            result = self.session.apicall('account.getImage', id = self['id'])
+            result = self.session.apicall('account.getImage', id=self['id'])
             data = result['result']['url']
         except RPCServerError as error:
             if error.code == "no_user_image":
@@ -43,16 +72,17 @@ class Account(Result):
     def __eq__(self, other):
         return other and self.get('id') == other.get('id')
 
+
 class MyAccount(Account):
     """Represents my account"""
 
     def getData(self):
         # extended = True needed to get user_type (admin, etc.).
-        result = self.session.apicall('account.self', 
-            extended = True,
-            org_settings = True,
-            user_applications = True,
-        )
+        result = self.session.apicall('account.self',
+                                      extended=True,
+                                      org_settings=True,
+                                      user_applications=True,
+                                      )
         return result['result']
 
     def user_applications(self):
